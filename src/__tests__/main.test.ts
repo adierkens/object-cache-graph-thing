7d2d85f5f6429b86c3168094860c6c40179f11cb
Adam Dierkens <adam@dierkens.com>
2018-10-30 23:13:13 -0700

    TS rewrite

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,57 +1,73 @@
-import test from 'ava';
-import _ from 'lodash';
-import ObjectCache, {isSubset} from '../main';
+import ObjectCache, { isSubset, Path } from '../main';
 
-test('isSubset', t => {
-  t.true(isSubset([1, 2, 3], [1, 2, 3]));
-  t.true(isSubset([1, 2, 3], [1, 2, 3, 4]));
-  t.false(isSubset([1, 2, 3], [1, 2]));
-  t.true(isSubset(['fields', 'asset'], ['fields', 'asset', 'values', 0]));
-  t.false(isSubset(['fields', 'asset'], ['field', 'asset', 'values', 0]));
-  t.false(isSubset('foo', ['foo', 'bar']));
+test('isSubset', () => {
+  expect(isSubset([1, 2, 3], [1, 2, 3])).toBeTruthy();
+  expect(isSubset([1, 2, 3], [1, 2, 3, 4])).toBeTruthy();
+  expect(isSubset([1, 2, 3], [1, 2])).toBeFalsy();
+  expect(
+    isSubset(['fields', 'asset'], ['fields', 'asset', 'values', 0])
+  ).toBeTruthy();
+  expect(
+    isSubset(['fields', 'asset'], ['field', 'asset', 'values', 0])
+  ).toBeFalsy();
+  expect(isSubset(('foo' as any) as Path, ['foo', 'bar'])).toBeFalsy();
 });
 
-const testObj = {
-  a: [{foo: true}, {foo: true, bar: {foo: false}}],
-  b: {
-    c: {
-      foo: true
+describe('objCache', () => {
+  const testObj = {
+    a: [{ foo: true }, { foo: true, bar: { foo: false } }],
+    b: {
+      c: {
+        foo: true
+      }
     }
-  }
-};
+  };
 
-const objCache = new ObjectCache(testObj, o => _.isObject(o) && _.has(o, 'foo') && o);
-const initialPaths = [['a', 0], ['a', 1], ['a', 1, 'bar'], ['b', 'c']];
+  const objCache = new ObjectCache(
+    testObj,
+    o => typeof o === 'object' && o.foo !== undefined && o
+  );
+  const initialPaths = [['a', 0], ['a', 1], ['a', 1, 'bar'], ['b', 'c']];
 
-test('Constructor does normal things with bad values', t => {
-  t.notThrows(() => {
-    let x = new ObjectCache();
-    x = new ObjectCache([]);
-    x = new ObjectCache(false);
-    t.true(x instanceof ObjectCache);
+  it('Constructor does normal things with bad values', () => {
+    expect(() => {
+      let x = new ObjectCache();
+      x = new ObjectCache([]);
+      x = new ObjectCache(false as any);
+      expect(x).toBeInstanceOf(ObjectCache);
+    }).not.toThrow();
   });
-});
 
-test('Constructs an ObjectCache with the right initial paths', t => {
-  t.deepEqual(objCache.getPaths(), initialPaths);
-});
+  it('Constructs an ObjectCache with the right initial paths', () => {
+    expect(objCache.getPaths()).toEqual(initialPaths);
+  });
 
-test('Constructs an ObjectCache with the right initial values', t => {
-  t.deepEqual(objCache.getValues(), [{foo: true}, {foo: true, bar: {foo: false}}, {foo: false}, {foo: true}]);
-});
+  it('Constructs an ObjectCache with the right initial values', () => {
+    expect(objCache.getValues()).toEqual([
+      { foo: true },
+      { foo: true, bar: { foo: false } },
+      { foo: false },
+      { foo: true }
+    ]);
+  });
 
-test('Constructs an ObjectCache from a relative path', t => {
-  const relativeToA = objCache.getCacheForPath(['a']);
-  t.deepEqual(relativeToA.getPaths(), [[0], [1], [1, 'bar']]);
-  t.deepEqual(relativeToA.getValues(), [{foo: true}, {foo: true, bar: {foo: false}}, {foo: false}]);
-});
+  it('Constructs an ObjectCache from a relative path', () => {
+    const relativeToA = objCache.getCacheForPath(['a']);
+    expect(relativeToA.getPaths()).toEqual([[0], [1], [1, 'bar']]);
+    expect(relativeToA.getValues()).toEqual([
+      { foo: true },
+      { foo: true, bar: { foo: false } },
+      { foo: false }
+    ]);
+  });
 
-test('Constructs an ObjectCache from an empty path', t => {
-  const identity = objCache.getCacheForPath([]);
-  t.deepEqual(identity.getPaths(), initialPaths);
-});
+  it('Constructs an ObjectCache from an empty path', () => {
+    const identity = objCache.getCacheForPath([]);
+    expect(identity.getPaths()).toEqual(initialPaths);
+  });
 
-test('Constructs an ObjectCache from a value', t => {
-  const fooCache = objCache.getCacheForValue({foo: true});
-  t.deepEqual(fooCache.getPaths(), [['a', 0], ['b', 'c']]);
+  it('Constructs an ObjectCache from a value', () => {
+    const fooCache = objCache.getCacheForValue({ foo: true });
+    expect(fooCache.getPaths()).toEqual([['a', 0], ['b', 'c']]);
+  });
 });
